1ac0ba57db8cfdf4e89ecbaf556cbbc1bdb7610a
BoneFuusen <kotov_sergey04@mail.ru>
2024-07-03 16:15:41 +0600

    Initial Commit

---
+++ IOSPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSPractice
+//
+//  Created by Сергей Котов on 03.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
