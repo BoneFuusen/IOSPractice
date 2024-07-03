e071bc4295ce0f21d92191b925bf8137c49f4448
BoneFuusen <kotov_sergey04@mail.ru>
2024-07-03 16:44:40 +0600

    init: start lab1 task

--- IOSPractice/ViewController.swift
+++ IOSPractice/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lazy var collectionView: UICollectionView = {
+            let layout = PagingCollectionViewLayout()
+            layout.sectionInset = .init(top:0, left:0, bottom:0, right:0)
+            
+            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            
+            return collectionView
+        }()
+        
     }
-
-
 }
-
